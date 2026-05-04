bdd04c184b05039f7430b2d392c8a57e2ad0e5ac
Atamn_Auto_CV_Maker <atmangcpcv@gmail.com>
2026-05-04 15:45:01 +0530

    google cloud sql

--- office_records/settings.py
+++ office_records/settings.py
@@ -58,12 +58,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'office_records.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
